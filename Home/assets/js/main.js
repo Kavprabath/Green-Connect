9fe6fbaf0c7adfc6b97ed0c583daab79ebfe2f8d
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2021-04-01 07:43:01 +0530

    completed post section #15

---
+++ Home/assets/js/main.js
@@ -0,0 +1,13 @@
+<script language="javascript">
+    function changeIcon() {
+
+        if (document.getElementById("imgClickAndChange").src == "./assets/img/heart.svg") 
+        {
+            document.getElementById("imgClickAndChange").src = "../img/like.svg";
+        }
+        else 
+        {
+            document.getElementById("imgClickAndChange").src = "./assets/img/heart.svg";
+        }
+    }
+</script>
