f67ee6d16d6116a1be68296835ec05f973fe31ec
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2021-05-06 16:42:08 +0530

    signup page firebase conversation

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,6 @@
+
+
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyB7gsFxhE7-9MpHM76OsN8jZHMrt7qBH4Q",
@@ -16,12 +19,29 @@ var firebaseConfig = {
   
   // Set database variable
   var database = firebase.database()
+
+
+  function stringToHash(string) {
+                  
+    var hash = 0;
+      
+    if (string.length == 0) return hash;
+      
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+      
+    return hash;
+}
   
   function signup() {
     
+    
+    var studentID = document.getElementById('student_id').value
     var fname = document.getElementById('user_fname').value
     var lname = document.getElementById('user_lname').value
-    var userName = document.getElementById('user_name').value
     var nsbmEmail = document.getElementById('nsbm_email').value
     var userDOB = document.getElementById('dob_input').value
     var userGender = document.getElementById('user_gender').value
@@ -29,22 +49,33 @@ var firebaseConfig = {
     var userFaculty = document.getElementById('user_faculty').value
     
     // Password Input Variables
-    var userPassword = document.getElementById('user_password').value
-    var userConfirmPassword = document.getElementById('confirm_password').value
+    var userPassword = stringToHash(document.getElementById('user_password').value);
+    var userConfirmPassword = stringToHash(document.getElementById('confirm_password').value);
+
+   //User Input Validations
+   if(studentID == "") {
+     alert("Please Fill Out the studentID!" );
+   }
+   
+
+
 
   
-    database.ref('Email/' + nsbmEmail).set({
+    database.ref('StudentId/' + studentID).set({
       first_name: fname,
       last_name: lname,
       user_dob: userDOB,
       user_gender: userGender,
       user_batch: userBatch,
       user_faculty: userFaculty,
+      user_email: nsbmEmail,
       user_password: userPassword
 
     })
   
     alert('User Registered Successfully!')
+    document.forms['input-sec'].reset()
+    
   }
   
   function get() {
